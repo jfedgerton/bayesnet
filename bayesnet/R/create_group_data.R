b014e386f46e3b40ab6513e37b498c99c9f59038
Jared F Edgerton <jared.edgerton@gmail.com>
2019-06-07 15:07:52 -0400

    Fixed nodefactor added ingroup
    Fixed some of the first stage variables for the model.

--- bayesnet/R/create_group_data.R
+++ bayesnet/R/create_group_data.R
@@ -14,21 +14,29 @@ create_group_data <- function(group.data, formula.stage.1)
   ## Turn the group data into inter and intra group data
   group.nodes <- nrow(as.matrix(group.data))
 
-  ## Create an adjacency matrix for output
+  ## Create an adjacency matrix for group data
   adj.mat <- matrix(0, nrow = group.nodes, ncol = group.nodes)
+
+  ## Names of all the groups
   colnames(adj.mat) <- rownames(adj.mat) <- group.data$Group_ID
+
+  ## For all groups
   group.data.for.analysis <- network::as.network(adj.mat, directed = F)
+
+  ## Turn data into a network
   group.dyads <- network::network.dyadcount(group.data.for.analysis)
   group.var.names <- colnames(group.data)
   group.var.names <- group.var.names[group.var.names != "Group_ID"]
 
-
+  ## Add in the network attributes
   for (vert.atts in 1:length(group.var.names))
   {
     group.data.for.analysis %v% group.var.names[vert.atts] <- group.data[,group.var.names[vert.atts]]
   }
 
-  if (grepl("absdiffcat", formula.stage.1) == T |
+  if (
+    ## Variables that cannot work for group data
+      grepl("absdiffcat", formula.stage.1) == T |
       grepl("transitive", formula.stage.1) == T |
       grepl("triad", formula.stage.1) == T |
       grepl("atleast", formula.stage.1) == T |
@@ -37,11 +45,12 @@ create_group_data <- function(group.data, formula.stage.1)
       grepl("dgwe", formula.stage.1) == T |
       grepl("edgecov", formula.stage.1) == T |
       grepl("kstar", formula.stage.1) == T |
-      grepl("absdiffcat", formula.stage.1) == T |
-      grepl("nodefactor", formula.stage.1) == T)
+      grepl("absdiffcat", formula.stage.1) == T)
   {
     cat("The program does not support one or more stage 1 coefficients.")
   } else {
+
+    ## Formula for group data
     group_form <- stats::as.formula(paste("group.data.for.analysis ~", formula.stage.1))
     dta.array.group <- ergm::ergmMPLE(group_form, output="array", maxMPLEsamplesize=+Inf,
                                       control=ergm::control.ergm(MPLE.max.dyad.types=group.dyads*10))
@@ -49,7 +58,7 @@ create_group_data <- function(group.data, formula.stage.1)
     ncoef.group <- length(dta.array.group$predictor[1,2,])
     dta.group <- matrix(0, nrow=group.dyads, ncol=3 + ncoef.group)
 
-
+    ## Creating group data
     idx.group <- 1
     for (tail.group in 1:(group.nodes-1)) {
       for (head.group in (tail.group+1):group.nodes) {
@@ -60,12 +69,13 @@ create_group_data <- function(group.data, formula.stage.1)
         idx.group <- idx.group+1
       }
     }
-
+    ## Name the group data
     dta.group <- data.frame(dta.group)
     nm.group <- c("Y", names(dta.array.group$predictor[tail.group, head.group, ]),
                   "Group1", "Group2")
     colnames(dta.group) <- nm.group
 
+    ## Creating group data
     all_groups <- unique(c(dta.group$Group1,dta.group$Group2))
     self_loop_data <- data.frame(matrix(NA, ncol = ncol(dta.group), nrow = length(all_groups)))
     colnames(self_loop_data) <- nm.group
@@ -73,16 +83,19 @@ create_group_data <- function(group.data, formula.stage.1)
     self_loop_data$Group1 <- all_groups
     self_loop_data$Group2 <- all_groups
 
+    ## Self loop data for groups
     for (self_loop in 2:ncol(self_loop_data))
     {
       if (grepl("nodematch.", colnames(self_loop_data)[self_loop]) == T)
       {
         self_loop_data[,colnames(self_loop_data)[self_loop]] <- 1
-      } else if (grepl("absdiff.", colnames(self_loop_data)[self_loop]) == T)
+      } else if (grepl("absdiff", colnames(self_loop_data)[self_loop]) == T)
       {
         self_loop_data[,colnames(self_loop_data)[self_loop]] <- 0
       }
     }
+
+    ## Add in the self loop data
     dta.group <- dplyr::bind_rows(dta.group,
                                   self_loop_data)
     dta.group <- dplyr::arrange(dta.group, Group1, Group2)
@@ -95,6 +108,81 @@ create_group_data <- function(group.data, formula.stage.1)
     dta.group[, "Group2"] <- Grp[,2]
     dta.group[, "Group_ID"] <- Grp[,3]
 
+    ## Fix factor variables
+    fix_factor_vars <- dta.group[,grepl("nodefactor", colnames(dta.group))]
+    factor_vars_form1 <- colnames(fix_factor_vars)
+    factor_vars_form1 <- gsub("nodefactor", "", factor_vars_form1)
+    factor_vars_form1 <- gsub("[[:digit:]]+", "", factor_vars_form1)
+    factor_vars_form1 <- unique(gsub("\\.", "", factor_vars_form1))
+
+    new_factor_variable_list <- list()
+    for (col_check_factor in 1:length(factor_vars_form1))
+    {
+        factor_permutations <- expand.grid(unique(group.data$Group_ID), unique(group.data$Group_ID))
+        factor_permutations$Keep <- factor_permutations$Var1 <= factor_permutations$Var2
+        factor_permutations <- subset(factor_permutations, Keep == T)
+        factor_permutations <- factor_permutations[,colnames(factor_permutations) != "Keep"]
+        colnames(factor_permutations) <- c("Group1", "Group2")
+        original_data_to_factorize <- group.data[,colnames(group.data) %in% c("Group_ID", factor_vars_form1[col_check_factor])]
+        factor_permutations <- merge(factor_permutations,
+                                     original_data_to_factorize,
+                                       by.x = "Group1",
+                                       by.y = "Group_ID",
+                                       all = T)
+
+        colnames(factor_permutations)[3] <- "Group1_Factor"
+
+        factor_permutations <- merge(factor_permutations,
+                                     original_data_to_factorize,
+                                     by.x = "Group2",
+                                     by.y = "Group_ID",
+                                     all = T)
+
+        colnames(factor_permutations)[4] <- "Group2_Factor"
+
+        factor_levels_to_create <- sort(unique(c(factor_permutations$Group1_Factor, factor_permutations$Group2_Factor)))
+        factor_levels_to_create <- factor_levels_to_create[2:length(factor_levels_to_create)]
+
+        new_col_names <- paste0("nodefactor.", factor_vars_form1[col_check_factor], ".",  factor_levels_to_create)
+
+        factorial_levels_list <- list()
+        for (check_rows in 1:length(factor_levels_to_create))
+        {
+          var_1_fac <- factor_permutations$Group1_Factor == factor_levels_to_create[check_rows]
+          var_2_fac <- factor_permutations$Group2_Factor == factor_levels_to_create[check_rows]
+
+          factorial_levels_list[[check_rows]] <- rowSums(cbind(var_1_fac, var_2_fac))
+        }
+
+        factorial_output <- do.call(cbind, factorial_levels_list)
+        colnames(factorial_output) <- new_col_names
+        factor_permutations <- cbind(factor_permutations, factorial_output)
+        factor_permutations <- factor_permutations[,colnames(factor_permutations) %in% c("Group1", "Group2", new_col_names)]
+
+        new_factor_variable_list[[col_check_factor]] <- factor_permutations
+    }
+    factor_permutations <- factor_permutations[,colnames(factor_permutations) %in% c("Group1", "Group2")]
+    for (list_length in 1:length(new_factor_variable_list))
+    {
+      factor_permutations <- merge(factor_permutations,
+                                   new_factor_variable_list[[list_length]],
+                                   by = c("Group1", "Group2"))
+    }
+
+    dta.group <- dta.group[,!(colnames(dta.group) %in% colnames(fix_factor_vars))]
+    factor_permutations$Group1 <- paste0("Group", factor_permutations$Group1)
+    factor_permutations$Group2 <- paste0("Group", factor_permutations$Group2)
+    dta.group <- merge(dta.group,
+                       factor_permutations,
+                       by = c("Group1", "Group2"))
+      }
+   }
+
+ if (grepl("ingroup", formula.stage.1))
+ {
+   dta.group$ingroup <- 0
+   dta.group$ingroup[dta.group$Group1 == dta.group$Group2] <- 1
+ }
     return(dta.group)
   }
 }
