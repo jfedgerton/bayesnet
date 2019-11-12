cb53ad15ca65c67df8b250602e3fc4bf0a181c83
Jared F Edgerton <jared.edgerton@gmail.com>
2019-11-12 04:43:40 -0500

    Changed the model priors

--- bayesnet/R/MCMC_sample.R
+++ bayesnet/R/MCMC_sample.R
@@ -133,8 +133,7 @@ par_sim_fun <- function(obj, chain_var) {
             bglmer(obj$form_sim,
                    data = sim_data,
                    family = binomial,
-                   cov.prior = NULL,
-                   fixef.prior = t())
+                   fixef.prior = NULL)
           }, silent = T)))
           
           if(class(cur_theta_temp) != "try-error")
